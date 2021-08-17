30e972e5e8daf21d9e297166c8f0277043edea65
Grove <grove1411@gmail.com>
2021-08-17 20:48:45 -0300

    MongoDB connection

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,20 @@
+// MongoDB connection
+
+const mongoose = require('mongoose')
+
+const stringMongo = 'mongodb+srv://Jocker:Xn4M4GLE1Omy2Rbq@grovecluster.kna71.mongodb.net/GroveCluster?retryWrites=true&w=majority'
+mongoose.connect(stringMongo, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.models = {}
+
+let accountSchema = new mongoose.Schema({
+    user: String,
+    email: String,
+    password: String 
+})
+
+let accounts = mongoose.model('accounts', accountSchema)
+
 module.exports = function(app){
     app.get('/login', (req, res)=>{
         res.render('login')
